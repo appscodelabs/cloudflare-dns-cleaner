216895ef9c952b38626312f596c42ee1899220e4
Tamal Saha <tamal@appscode.com>
2023-07-26 07:57:45 -0700

    Add delete option
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- README.md
+++ README.md
@@ -11,7 +11,11 @@ go install github.com/appscodelabs/cloudflare-dns-cleaner@latest
 ## Usage
 
 ```bash
-export CLOUDFLARE_API_TOKEN=****
+export CLOUDFLARE_API_TOKEN=***
 
+# list records
 cloudflare-dns-cleaner --fqdn=example.com
+
+# delete records
+cloudflare-dns-cleaner --fqdn=example.com --delete
 ```
--- main.go
+++ main.go
@@ -13,14 +13,23 @@ import (
 
 func main() {
 	fqdn := ""
+	del := false
 	flag.StringVar(&fqdn, "fqdn", fqdn, "Domain name")
+	flag.BoolVar(&del, "delete", del, "If true, deletes DNS records")
 	flag.Parse()
 
 	if fqdn == "" {
 		panic("set --fqdn flag")
 	}
 
-	ListDNSRecords(fqdn, false)
+	if del {
+		// ask for confirmation
+		fmt.Println("Are you sure you want to delete DNS records? [Y/N]")
+		if !askForConfirmation() {
+			os.Exit(0)
+		}
+	}
+	ListDNSRecords(fqdn, del)
 }
 
 func ListDNSRecords(fqdn string, del bool) error {
@@ -61,4 +70,47 @@ func ListDNSRecords(fqdn string, del bool) error {
 		}
 	}
 	return nil
+}
+
+// ref: https://gist.github.com/albrow/5882501
+
+// askForConfirmation uses Scanln to parse user input. A user must type in "yes" or "no" and
+// then press enter. It has fuzzy matching, so "y", "Y", "yes", "YES", and "Yes" all count as
+// confirmations. If the input is not recognized, it will ask again. The function does not return
+// until it gets a valid response from the user. Typically, you should use fmt to print out a question
+// before calling askForConfirmation. E.g. fmt.Println("WARNING: Are you sure? (yes/no)")
+func askForConfirmation() bool {
+	var response string
+	_, err := fmt.Scanln(&response)
+	if err != nil {
+		panic(err)
+	}
+	okayResponses := []string{"y", "Y", "yes", "Yes", "YES"}
+	nokayResponses := []string{"n", "N", "no", "No", "NO"}
+	if containsString(okayResponses, response) {
+		return true
+	} else if containsString(nokayResponses, response) {
+		return false
+	} else {
+		fmt.Println("Please type yes or no and then press enter:")
+		return askForConfirmation()
+	}
+}
+
+// You might want to put the following two functions in a separate utility package.
+
+// posString returns the first index of element in slice.
+// If slice does not contain element, returns -1.
+func posString(slice []string, element string) int {
+	for index, elem := range slice {
+		if elem == element {
+			return index
+		}
+	}
+	return -1
+}
+
+// containsString returns true iff slice contains element
+func containsString(slice []string, element string) bool {
+	return !(posString(slice, element) == -1)
 }
